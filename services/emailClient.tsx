8ca631eedac83d116ea386f43dd6073dc6ce13fb
Krzysztof Tomaszewski <drteski@gmail.com>
2026-01-15 10:20:57 +0100

    init

--- services/emailClient.tsx
+++ services/emailClient.tsx
@@ -3,7 +3,7 @@ import prisma from '@/lib/db';
 import { render } from '@react-email/components';
 import { EmailMessage } from '@/components/EmailMessage';
 import { Report } from '@/types/types';
-import { format } from 'date-fns';
+import { format, subDays } from 'date-fns';
 
 export const sendEmail = async (data: Report): Promise<string> => {
 	const settings = await prisma.mailing.findFirst();
@@ -37,7 +37,7 @@ export const sendEmail = async (data: Report): Promise<string> => {
 		from,
 		to,
 		cc: udw,
-		subject: `${subject} - ${format(new Date(), 'dd-MM-yyyy')}`,
+		subject: `${subject} - ${format(subDays(new Date(), 1), 'dd-MM-yyyy')}`,
 		html: emailHtml
 	});
 	return message.envelope.to[0];
